dc7fc1f18092e4e59bca6ceb43ea7c0aafdab3a8
avanthika <avanthikapraveen7@gmail.com>
2025-07-10 15:05:05 +0530

    Added the YT Comment fetcher using Data API

--- youtube_utils.py
+++ youtube_utils.py
@@ -10,8 +10,24 @@ def extract_video_id(url):
     match = re.search(r"(?:v=|\/)([0-9A-Za-z_-]{11})", url)
     return match.group(1) if match else None
 
-print(extract_video_id("https://www.youtube.com/watch?v=abc123XYZ_9"))  # abc123XYZ_9
-print(extract_video_id("https://youtu.be/abc123XYZ_9"))                 # abc123XYZ_9
-print(extract_video_id("https://youtube.com/shorts/abc123XYZ_9"))      # abc123XYZ_9
-print(extract_video_id("https://notyoutube.com/watch?v=abc123XYZ_9"))  # abc123XYZ_9 (but maybe shouldn't!)
-print(extract_video_id("invalid_url"))                                  # None
+def get_video_comments(video_url, max_comments = 50):
+    video_id = extract_video_id(video_url)
+    if not video_id:
+        raise ValueError("Invalid Youtube video URL")
+    youtube = build("youtube", "v3", developerKey = YOUTUBE_API_KEY)
+    comments = []
+    next_page_token = None
+    while len(comments) < max_comments:
+        response = youtube.commentThreads().list( 
+            part = 'snippet',
+            videoId = video_id,
+            maxResults = min(100, max_comments - len(comments)),
+            pageToken = next_page_token
+        ).execute()
+        for item in response["items"]:
+            comment = item["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
+            comments.append(comment)
+        next_page_token = response.get("nextPageToken")
+        if not next_page_token:
+            break
+    return comments
